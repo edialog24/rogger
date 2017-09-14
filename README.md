789c28eab5c67b73f949a28cea5af6041b78ad1c
mrcox007 <kenneth.rinnan@gmail.com>
2017-09-14 11:36:58 +0200

    First commmit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rogger
+
+Rogger is a library that uses our winston-amqplib for sending messages to rabbit, it's mainly used for logging into the elk stack.
+
---
+++ examples/index.js
@@ -0,0 +1,5 @@
+require('../lib/index');
+Rogger.configure();
+Rogger.log('info','Global');
+var blogger = new Rogger.Logger();
+blogger.log('info','first logger');
