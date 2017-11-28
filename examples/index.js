9147f34d90aa813e3164475ccc2270633fb66153
Håkon Solberg <hakonsol@gmail.com>
2017-11-28 12:32:11 +0100

    Handling for error object (exception), prints error msg and stack. Also added usage example in examples.

--- examples/index.js
+++ examples/index.js
@@ -3,5 +3,11 @@ require('../lib/index')({url:'amqp://localhost'},{level:'debug'})
         Rogger.log('info','Global logger');
         let blogger = new Rogger.Logger();
         blogger.log('info','Custom logger');
+        // Printing error exception with error stack
+        let err = new Error("Error message");
+        Rogger.error(err);
+        // The explicit alternative (same result) is:
+        Rogger.error(err.message, {error: err.stack.toString()});
+
     })
     .catch((err) => {console.error(err);});
